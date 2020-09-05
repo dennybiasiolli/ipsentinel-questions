d3d6d67852c3b989255764d4f34ad6b9afc77d8f
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2020-09-05 17:45:52 +0200

    completing question 1

---
+++ src/q1.py
@@ -0,0 +1,16 @@
+def fizz_buzz():
+    for i in range(1, 101):
+        s = ''
+        if i % 3 == 0 or '3' in str(i):
+            s = 'Fizz'
+        if i % 5 == 0 or '5' in str(i):
+            s = ' '.join((s, 'Buzz')).lstrip()
+        print(i, s)
+
+
+def main():
+    fizz_buzz()
+
+
+if __name__ == "__main__":
+    main()
